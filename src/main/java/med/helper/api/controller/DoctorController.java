2519b1b626fac7396722aa576223f75cb31b5244
iagoliziero <iagolizierox14@gmail.com>
2025-08-18 22:23:10 -0300

    wip: sending data to DoctorRepository

--- src/main/java/med/helper/api/controller/DoctorController.java
+++ src/main/java/med/helper/api/controller/DoctorController.java
@@ -1,6 +1,9 @@
 package med.helper.api.controller;
 
-import med.helper.api.doctor.RegisterDataDoctor;
+import med.helper.api.models.doctor.Doctor;
+import med.helper.api.models.doctor.DoctorRepository;
+import med.helper.api.models.doctor.RegisterDataDoctor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,8 +13,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/doctor")
 public class DoctorController {
 
+    @Autowired
+    private DoctorRepository repository;
+
     @PostMapping
     public void registerDoctor(@RequestBody RegisterDataDoctor data) {
-        System.out.println(data);
+        repository.save(new Doctor(data));
     }
 }
---
+++ src/main/java/med/helper/api/models/doctor/DoctorRepository.java
@@ -0,0 +1,7 @@
+package med.helper.api.models.doctor;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface DoctorRepository extends JpaRepository<Doctor, Long> {
+
+}
