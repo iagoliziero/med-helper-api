d1e24653f6a90e4771206f8fbe6c8180defcc1fe
iagoliziero <iagolizierox14@gmail.com>
2025-10-31 21:46:40 -0300

    feat: recover, validation and authenticating User

--- src/main/java/med/helper/api/infra/security/SecurityConfigurations.java
+++ src/main/java/med/helper/api/infra/security/SecurityConfigurations.java
@@ -2,6 +2,7 @@ package med.helper.api.infra.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
@@ -18,9 +19,14 @@ import org.springframework.security.web.SecurityFilterChain;
 public class SecurityConfigurations {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        return http.csrf(AbstractHttpConfigurer::disable)
-                .sessionManagement(sm -> sm.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-                .build();
+        return
+            http.csrf(csrf -> csrf.disable())
+                    .sessionManagement(sm -> sm.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                    .authorizeHttpRequests(req -> {
+                        req.requestMatchers("/login").permitAll();
+                        req.anyRequest().authenticated();
+                    })
+                    .build();
     }
 
     @Bean
--- src/main/java/med/helper/api/infra/security/TokenService.java
+++ src/main/java/med/helper/api/infra/security/TokenService.java
@@ -3,6 +3,7 @@ package med.helper.api.infra.security;
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
 import com.auth0.jwt.exceptions.JWTCreationException;
+import com.auth0.jwt.exceptions.JWTVerificationException;
 import med.helper.api.domain.user.User;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
@@ -27,6 +28,19 @@ public class TokenService {
                     .sign(algorithm);
         } catch (JWTCreationException exception){
             throw new RuntimeException("Error generate token JWT", exception);
+        }
+    }
+
+    public String getSubject(String tokenJWT) {
+        try {
+            var algorithm = Algorithm.HMAC256(secret);
+            return JWT.require(algorithm)
+                    .withIssuer("API Med.helper")
+                    .build()
+                    .verify(tokenJWT)
+                    .getSubject();
+        } catch (JWTVerificationException exception){
+            throw new RuntimeException("Token invalid or expired!");
         }
     }
 
